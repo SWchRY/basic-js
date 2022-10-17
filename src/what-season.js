1a3b5e3f5364b94aeb396c41202d5fb9ae4272c1
SwchRY <urkevich.alex.chr@mail.ru>
2022-10-17 14:47:15 +0300

    feat: solved 4th

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  //const Pdate = new Date(2022,02,31)
+  if(!date){
+    return 'Unable to determine the time of year!'
+  }
+  if(!date) return 'Unable to determine the time of year!'
+  if(date.getDate != Date.prototype.getDate) throw new Error('Invalid date!') 
+  if (Object.getOwnPropertyNames(date).length > 0) {
+    throw Error('Invalid date!');
+  }
+
+  let month = date.getMonth() + 1
+  let year = date.getYear() + 1900
+  let day = date.getDate()
+  if(isNaN(day) || isNaN(month) || isNaN(year)) throw Error('Invalid date!')
+  const NowDate = new Date()
+  let invalid = Object.keys(date)
+  if(!(date instanceof Date) || (!date.getTime) || (invalid.length > 0)) throw Error('Invalid date!')
+  if(date.getTime()===NowDate.getTime()) return 'Unable to determine the time of year!'
+  const months = ['spring','summer','autumn','winter']
+  if(month >= 3 && month <= 5) return months[0]
+  else if(month >= 6 && month <= 8) return months[1]
+  else if(month >= 9 && month <= 11) return months[2]
+  else if(month == 12 || month == 1 || month == 2) return months[3]
+  else return false
+  
 }
 
 module.exports = {
